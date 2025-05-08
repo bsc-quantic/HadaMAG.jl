d6df785741ee8c73dec5af00926e688de5623f65
Documenter.jl <documenter@juliadocs.github.io>
2025-05-08 13:23:02 +0000

    build based on 33878e9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HadaMAG.jl-Documentation","page":"Home","title":"HadaMAG.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Example:","category":"page"},{"location":"#HadaMAG.func-Tuple{Any}","page":"Home","title":"HadaMAG.func","text":"func(x)\n\nReturn double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
+}
