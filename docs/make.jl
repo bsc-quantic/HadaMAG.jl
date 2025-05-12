fcf1b782b9f827cdcda567741f3b943be844ae4f
jofrevalles <jofrevalles99@gmail.com>
2025-05-12 11:31:21 +0200

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,16 +3,15 @@ using HadaMAG
 
 makedocs(
     modules = [HadaMAG],
-    sitename="HadaMAG.jl",
-    pages = Any[
-        "Home" => "index.md",
-        "Manual" => [
-            "State" => "manual/State.md",
-        ]
-    ],
+    sitename = "HadaMAG.jl",
+    pages = Any["Home"=>"index.md", "Manual"=>["State"=>"manual/State.md", "Helpers"=>"manual/Helpers.md"]],
     pagesonly = true,
     format = Documenter.HTML(),
     warnonly = true,
 )
 
-deploydocs(; repo="github.com/bsc-quantic/HadaMAG.jl.git", devbranch="master", push_preview=true)
+deploydocs(;
+    repo = "github.com/bsc-quantic/HadaMAG.jl.git",
+    devbranch = "master",
+    push_preview = true,
+)
