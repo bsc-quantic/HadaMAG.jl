f2aa52ed9a1c2e66546e9d935dedcf65bb1de5df
jofrevalles <jofrevalles99@gmail.com>
2025-05-16 09:21:05 +0200

    Update Backends folder

---
+++ src/Backends/Dispatch.jl
@@ -0,0 +1,52 @@
+include("Serial.jl")
+include("Threads.jl")
+
+abstract type AbstractBackend end
+struct Serial <: AbstractBackend end
+struct Threaded <: AbstractBackend end
+struct MPIThreads <: AbstractBackend end  # defined even if MPI absent
+
+# ---------- backend selector ----------
+function _choose_backend(b::Symbol)
+    if b === :serial
+        Serial()
+    elseif b === :threads
+        Threaded()
+    elseif b === :mpi_threads
+        @assert isdefined(Main, :MPI) "MPI backend requested but MPI.jl is not loaded."
+        MPIThreads()
+    else
+        _auto_backend()
+    end
+end
+
+function _auto_backend()
+    if isdefined(Main, :MPI)
+        MPIThreads()
+    elseif Threads.nthreads() > 1
+        Threaded()
+    else
+        Serial()
+    end
+end
+
+# ---------- backend call helper ----------
+_apply_backend(::Serial, fsym, args...; kw...) =
+    getfield(HadaMAG.SerialBackend, fsym)(args...; kw...)
+
+_apply_backend(::Threaded, fsym, args...; kw...) =
+    getfield(HadaMAG.ThreadsBackend, fsym)(args...; kw...)
+
+_apply_backend(::MPIThreads, fsym, args...; kw...) =
+    if !isnothing(Base.get_extension(HadaMAG, :HadaMAGMPIExt))
+        getfield(Base.get_extension(HadaMAG, :HadaMAGMPIExt), fsym)(args...; kw...)
+    else
+        throw(ArgumentError("MPI backend unavailable – `using MPI` first"))
+    end
+
+# _apply_backend(::MPIThreads, fsym, args...; kw...) =
+#     if isdefined(HadaMAG, :MPIThreadsBackend)
+#         getfield(HadaMAG.MPIThreadsBackend, fsym)(args...; kw...)
+#     else
+#         throw(ArgumentError("MPI backend unavailable – `using MPI` first"))
+#     end
--- src/HadaMAG.jl
+++ src/HadaMAG.jl
@@ -6,7 +6,7 @@ include("State.jl")
 export StateVec,
     load_state, rand_haar, apply_2gate!, apply_2gate, qubits, qudits, qudit_dim, data
 
-include("backends/Dispatch.jl")
+include("Backends/Dispatch.jl")
 
 include("SRE2.jl")
 export MC_SRE2
